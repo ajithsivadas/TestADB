e2186faa5784923176aa05424d781130c307bd99
Ajith Sivadas <43530950+ajithsivadas@users.noreply.github.com>
2022-02-02 17:21:57 -0600

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ app = Flask(__name__)
  
 @app.route('/hello')
 def hello():
-    return 'Ajith Sivadas - 1001829098'
+    return 'Ajith Sivadas - 1001829098'<img src="https://asss1.blob.core.windows.net/container1/chuck.jpg"/>
  
 #app.run(host='localhost', port=5000)
